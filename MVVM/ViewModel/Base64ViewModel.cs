560d8e2cf8046f598cc05ac7c0f08ea4d824460d
Nikita Yarosh <ThRuWol@yandex.ru>
2021-06-27 17:39:39 +0300

    mvvm

---
+++ MVVM/ViewModel/Base64ViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cipher_Шифер_.MVVM.ViewModel
+{
+    class Base64ViewModel
+    {
+    }
+}
